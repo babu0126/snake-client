f48d2ee2fca2a0c9196fdc099fc2bca16d58ad98
David Wang <18davidwang@gmail.com>
2022-10-26 17:08:18 -0700

    set the connection on the client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require('net');
+const DATA = 'data';
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.1.65', // IP address
+    port: 50541 // Port number
+  });
+  conn.setEncoding("utf8");
+  conn.on(DATA, (data) => {
+    console.log('Incoming Data ...', data);
+  });
+
+return conn;
+
+};
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,17 +1,6 @@
 const net = require('net');
+const connect = require('./client');
 const DATA = 'data';
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.65', // IP address
-    port: 50541 // Port number
-  });
-  conn.setEncoding("utf8");
-  conn.on(DATA, (data) => {
-    console.log('Incoming Data ...', data);
-  });
 
-return conn;
-
-};
-console.log("Connecting ...");
 connect();
+console.log("Connecting ...");
